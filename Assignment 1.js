685d70082dfc70f35d09f16f05dc2476fde97af7
jsharmi18421245 <70466945+jsharmi18421245@users.noreply.github.com>
2020-09-02 22:02:54 +0530

    Assignment-1 for Day-2 (Javascript)
    Assignment

---
+++ Assignment 1.js
@@ -0,0 +1,22 @@
+// Q.No.1 program to search for a particular character in a string
+let str = ("Welcome to Javascript");
+let res = str.charAt(5);
+console.log(res);
+
+//Q.No.2 Program to convert minutes to seconds 
+let minutes = 5;
+let seconds = minutes*60;
+console.log("seconds="+seconds);
+
+//Q.No.3 Program to search for a element in a array of string
+let arr=["C language","Javascript","python","R"];
+let a1 = arr.indexOf("R");
+console.log(a1);
+
+//Q.No.4 Program to display only elements containin 'a' in them from a array
+let a = ["Ant","Man","Hate","Mate","go"];
+console.log("array=" + a);
+
+//Q.No.5 Print an array in reverse order
+let c = ["God","Jesus","Sharmila","Holy"];
+console.log("rev of c="+c.reverse());
